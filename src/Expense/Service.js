e2b41a0187a2bd2751fdd6400b72542ca2ce8427
Faheem <mdfaheem.aitrich@gmail.com>
2024-03-22 15:35:44 +0530

    feat: Category updated

--- src/Expense/Service.js
+++ src/Expense/Service.js
@@ -1,32 +1,50 @@
 
+import ExpenseCategory from "../Models/ExpenseCategoryModel.js";
 import Expense from "../Models/ExpenseModel.js"
 import User from "../Models/UserModel.js";
 // import moment from "moment";
 
 // Add Expense
 const addExpense = async (userId, expenseData) => {
-    try {
-        const finduser = await User.findById(userId)
-        if (finduser) {
-            expenseData.user = {
-                userId: finduser._id,
-                lastName: finduser.lastname,
-                password: finduser.password,
-                userName: finduser.username,
-                firstName: finduser.firstname,
-                phone: finduser.phone,
-                email:finduser.email
-            }
-            const postexpense = await Expense.create(expenseData)
-            if (postexpense) {
-                return postexpense
-            }
-        }
-    } catch (error) {
-        console.log(error);
-        throw error;
+  try {
+    const finduser = await User.findById(userId);
+    if (finduser) {
+      // Embed category data
+      expenseData.user = {
+        userId: finduser._id,
+        lastName: finduser.lastname,
+        password: finduser.password,
+        userName: finduser.username,
+        firstName: finduser.firstname,
+        phone: finduser.phone,
+        email: finduser.email,
+      };
+
+      // Finding category
+      const category = await ExpenseCategory.findOne({
+        name: expenseData.category.name,
+      });
+
+      if (!category) {
+        throw new Error("Category not found");
+      }
+
+      expenseData.category = {
+        name: category.title,
+        description: category.description,
+      };
+
+      const postexpense = await Expense.create(expenseData);
+      if (postexpense) {
+        return postexpense;
+      }
     }
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
+
 
 
 
--- src/Models/IncomeModel.js
+++ src/Models/IncomeModel.js
@@ -1,52 +1,58 @@
 import mongoose from "mongoose"; // Importing Mongoose
 
 const incomeSchema = new mongoose.Schema(
-    {    user:{
-        userId: mongoose.Schema.Types.ObjectId,
-        firstName: String,
-        lastName: String,
-        email: String,
-        phone: Number,
-        userName: String,
-        password:String,  
-       },
-       
-        title: {
-            type: String,
-            required: true,
-            trim: true,
-            maxLength:50
-        },
-        amount: {
-            type: Number,
-            required: true,
-            maxLength: 20,
-            trim: true
-        },
-        type: {
-            type: String,
-            default: "Income"
-        },
-        date : {
-            type: String,
-            required: true,
-            trim: true
-        },
-        category: {
-            type: String,
-            required: true,
-            trim: true
-            
-        },
-        description: {
-            type: String,
-            required: true,
-            maxLength: 20,
-            trim: true
-        }
-
+  {
+    user: {
+      userId: mongoose.Schema.Types.ObjectId,
+      firstName: String,
+      lastName: String,
+      email: String,
+      phone: Number,
+      userName: String,
+      password: String,
     },
-    {versionKey:false}
+
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 50,
+    },
+    amount: {
+      type: Number,
+      required: true,
+      maxLength: 20,
+      trim: true,
+    },
+    type: {
+      type: String,
+      default: "Income",
+    },
+    date: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    category: {
+      name: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      description: {
+        type: String,
+        trim: true,
+        maxLength: 100,
+      },
+    },
+    description: {
+      type: String,
+      required: true,
+      maxLength: 20,
+      trim: true,
+    },
+  },
+  { versionKey: false }
 );
 
 const Income =  mongoose.model("Income",incomeSchema)
