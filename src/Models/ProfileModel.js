9a3367d70ccc65f39275c80de1db2024efb10a40
divyasushinanay <divyasushin.aitrich@gmail.com>
2024-03-28 10:49:51 +0530

    profileupdated

--- src/Models/ProfileModel.js
+++ src/Models/ProfileModel.js
@@ -3,7 +3,15 @@ import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
 const ProfileSchema = new Schema(
-  {
+  {  user: {
+    userId: mongoose.Schema.Types.ObjectId,
+    firstName: String,
+    lastName: String,
+    email: String,
+    phone: Number,
+    userName: String,
+    password: String,
+  },
     imageUrls: {
       type: String,
       required: true,
--- src/Profile/Service.js
+++ src/Profile/Service.js
@@ -1,80 +1,3 @@
-// import User from "../Models/UserModel.js";
-// import Profile from "../Models/ProfileModel.js";
-// // import logger from "../Middleware/logger.js"
-
-// const uploadProfilePicture = async (file, userId) => {
-//   try {
-//     // Check if the user exists
-//     const existingUser = await User.findById(userId);
-//     if (!existingUser) {
-//       throw new Error("User not found");
-//     }
-
-//     // Create or update the profile picture for the user
-//     existingUser.profilePicture = {
-//       title: file.originalname,
-//       profilePicture: file.path,
-//     };
-
-//     // Save the user with the updated profile picture
-//     await existingUser.save();
-
-//     logger.info("Profile Picture uploaded successfully");
-//   } catch (error) {
-//     logger.error("Error uploading profile picture:", error);
-//     throw error;
-//   }
-// };
-
-// export default { uploadProfilePicture };
-
-// import User from "../Models/UserModel.js";
-// import logger from "../Middleware/logger.js";
-// import Profile from "../Models/ProfileModel.js";
-
-
-// const uploadProfilePicture = async (file, userId) => {
-//   try {
-//     const existingUser = await User.findById(userId);
-//     if (!existingUser) {
-//       throw new Error("User not found");
-//     }
-
-//     await Profile.create({imageUrls: file.path})
-
-//     logger.info("Profile Picture uploaded successfully");
-//   } catch (error) {
-//     logger.error("Error uploading profile picture:", error);
-//     throw error;
-//   }
-// };
-
-// const getprofilepicture = async(userId)=>{
-//   try {
-//         const existinguser= await User.findById(userId)
-//     if (existinguser) {
-       
-//         const profilepicture =  existinguser.ProfilePicture
-//         if (profilepicture) {
-          
-//           logger.info("profile picture is getting successfully")
-//           return  profilepicture
-//         }
-//       } else {
-//         logger.error("job  profile not found with specific id")
-       
-//       }
-//     } 
-//     catch (error) {
-//       logger.error("Error uploading profile picture:", error);
-//       throw error;
-//     }
-  
-// };
-
-
-// export default { uploadProfilePicture,getprofilepicture };
-
 import User from "../Models/UserModel.js";
 import logger from "../Middleware/logger.js";
 import Profile from "../Models/ProfileModel.js";
@@ -87,7 +10,21 @@ const uploadProfilePicture = async (file, userId) => {
     }
 
     // Create or update the profile picture for the user
-    await Profile.create({ imageUrls: file.path });
+    const profile = await Profile.findOneAndUpdate(
+      { "user.userId": userId },
+      {
+        $set: {
+          "user.firstName": existingUser.firstname,
+          "user.lastName": existingUser.lastname,
+          "user.email": existingUser.email,
+          "user.phone": existingUser.phone,
+          "user.userName": existingUser.username,
+          "user.password": existingUser.password,
+          imageUrls: file.path,
+        }
+      },
+      { upsert: true, new: true }
+    );
 
     logger.info("Profile Picture uploaded successfully");
   } catch (error) {
@@ -96,22 +33,7 @@ const uploadProfilePicture = async (file, userId) => {
   }
 };
 
-// const getProfilePicture = async (userId) => {
-//   try {
-//     const profile = await Profile.findOne({ userId: userId });
-//     if (!profile) {
-//       throw new Error("Profile not found for this user");
-//     }
 
-//     logger.info("Profile picture retrieved successfully");
-//     return "1711427386578__about-2.jpg";
-   
-
-//   } catch (error) {
-//     logger.error("Error retrieving profile picture:", error);
-//     throw error;
-//   }
-// };
 
 
 const getProfilePicture = async (userId) => {
@@ -137,3 +59,69 @@ const getProfilePicture = async (userId) => {
 
 
 export default { uploadProfilePicture, getProfilePicture };
+
+
+
+// -----------------------------------------------------------------------------------
+// import User from "../Models/UserModel.js";
+// import logger from "../Middleware/logger.js";
+// import Profile from "../Models/ProfileModel.js";
+
+// const uploadProfilePicture = async (file, userId) => {
+//   try {
+//     const existingUser = await User.findById(userId);
+//     if (!existingUser) {
+//       throw new Error("User not found");
+//     }
+
+//     // Create or update the profile picture for the user
+//     await Profile.create({ imageUrls: file.path });
+
+//     logger.info("Profile Picture uploaded successfully");
+//   } catch (error) {
+//     logger.error("Error uploading profile picture:", error);
+//     throw error;
+//   }
+// };
+
+// // const getProfilePicture = async (userId) => {
+// //   try {
+// //     const profile = await Profile.findOne({ userId: userId });
+// //     if (!profile) {
+// //       throw new Error("Profile not found for this user");
+// //     }
+
+// //     logger.info("Profile picture retrieved successfully");
+// //     return "1711427386578__about-2.jpg";
+   
+
+// //   } catch (error) {
+// //     logger.error("Error retrieving profile picture:", error);
+// //     throw error;
+// //   }
+// // };
+
+
+// const getProfilePicture = async (userId) => {
+//   try {
+//     // Find the profile document in the database for the given userId
+//     const profile = await Profile.findOne({ userId: userId });
+//     if (!profile) {
+//       throw new Error("Profile not found for this user");
+//     }
+
+//     // Extract the imageUrl from the profile document
+//     const imageUrl = profile.imageUrls;
+
+//     logger.info("Profile picture retrieved successfully");
+
+//     // Return the actual imageUrl from the database
+//     return imageUrl;
+//   } catch (error) {
+//     logger.error("Error retrieving profile picture:", error);
+//     throw error;
+//   }
+// };
+
+
+// export default { uploadProfilePicture, getProfilePicture };
